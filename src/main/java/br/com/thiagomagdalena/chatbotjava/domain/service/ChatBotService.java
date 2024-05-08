6f861b6b5ed7d4f6288496b8c78c38d7d7bfffb8
thiagomag <thiagomagdalena@gmail.com>
2024-05-08 20:46:22 -0300

    continuing the code

--- src/main/java/br/com/thiagomagdalena/chatbotjava/domain/service/ChatBotService.java
+++ src/main/java/br/com/thiagomagdalena/chatbotjava/domain/service/ChatBotService.java
@@ -2,6 +2,8 @@ package br.com.thiagomagdalena.chatbotjava.domain.service;
 
 import br.com.thiagomagdalena.chatbotjava.infra.openai.DadosRequisicaoChatCompletion;
 import br.com.thiagomagdalena.chatbotjava.infra.openai.OpenAIClient;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
+import io.reactivex.Flowable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ public class ChatBotService {
 
     private final OpenAIClient openAIClient;
 
-    public String responderPergunta(String pergunta) {
+    public Flowable<ChatCompletionChunk> responderPergunta(String pergunta) {
         var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
         var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
         return openAIClient.enviarRequisicaoChatCompletion(dados);
--- src/main/java/br/com/thiagomagdalena/chatbotjava/web/controller/ChatController.java
+++ src/main/java/br/com/thiagomagdalena/chatbotjava/web/controller/ChatController.java
@@ -2,9 +2,12 @@ package br.com.thiagomagdalena.chatbotjava.web.controller;
 
 import br.com.thiagomagdalena.chatbotjava.domain.service.ChatBotService;
 import br.com.thiagomagdalena.chatbotjava.web.dto.PerguntaDto;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
+import io.reactivex.Flowable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,8 +25,16 @@ public class ChatController {
 
     @PostMapping
     @ResponseBody
-    public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return chatBotService.responderPergunta(dto.pergunta());
+    public ResponseBodyEmitter responderPergunta(@RequestBody PerguntaDto dto) {
+        var fluxoResponsta = chatBotService.responderPergunta(dto.pergunta());
+        var emitter = new ResponseBodyEmitter();
+        fluxoResponsta.subscribe(chunck -> {
+            var token = chunck.getChoices().get(0).getMessage().getContent();
+            if (token != null) {
+                emitter.send(token);
+            }
+        }, emitter::completeWithError, emitter::complete);
+        return emitter;
     }
 
     @GetMapping("limpar")
