cfca554ae62f87a57a6e422d49c38e534f4bf779
thiagomag <thiagomagdalena@gmail.com>
2024-05-08 22:36:49 -0300

    changes for using assistant

--- src/main/java/br/com/thiagomagdalena/chatbotjava/domain/service/ChatBotService.java
+++ src/main/java/br/com/thiagomagdalena/chatbotjava/domain/service/ChatBotService.java
@@ -2,8 +2,6 @@ package br.com.thiagomagdalena.chatbotjava.domain.service;
 
 import br.com.thiagomagdalena.chatbotjava.infra.openai.DadosRequisicaoChatCompletion;
 import br.com.thiagomagdalena.chatbotjava.infra.openai.OpenAIClient;
-import com.theokanning.openai.completion.chat.ChatCompletionChunk;
-import io.reactivex.Flowable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +11,7 @@ public class ChatBotService {
 
     private final OpenAIClient openAIClient;
 
-    public Flowable<ChatCompletionChunk> responderPergunta(String pergunta) {
+    public String responderPergunta(String pergunta) {
         var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
         var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
         return openAIClient.enviarRequisicaoChatCompletion(dados);
